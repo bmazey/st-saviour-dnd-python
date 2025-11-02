a6f16e3133ab663d7b12958451829ad39ada36e3
brandon mazey <brandon@brandons-MacBook-Air.local>
2025-11-02 16:23:59 -0500

    cleaning up

--- game.py
+++ game.py
@@ -1,36 +1,7 @@
 import random
 import time
 
-from tav import Tav
-
-def generate_monster() -> int:
-    r = random.randint(1, 6)
-    if r == 1 or r == 2 or r == 3:
-        pixie = '\U0001F9DA'
-        print('+++++++ ' + pixie + ' A SWARM OF PIXIES ' + pixie + ' +++++++')
-        print('+                                     +')
-        print('+          roll required:  8          +')
-        print('+                                     +')
-        print('+++++++++++++++++++++++++++++++++++++++')
-        return 8
-    if r == 4 or r == 5:
-        mask = '\U0001F3AD'
-        print('++++++++ ' + mask + ' DISGUISED MIMIC ' + mask + ' ++++++++')
-        print('+                                     +')
-        print('+          roll required:  12         +')
-        print('+                                     +')
-        print('+++++++++++++++++++++++++++++++++++++++')
-        return 12
-    if r == 6:
-        eye = '\U0001F441'
-        print('+++++++++ ' + eye + '  EVIL BEHOLDER ' + eye + '  +++++++++')
-        print('+                                     +')
-        print('+          roll required:  18         +')
-        print('+                                     +')
-        print('+++++++++++++++++++++++++++++++++++++++')
-        return 18
-
-    return -1
+from draw import draw_d20, draw_d6, draw_d4
 
 def print_dramatic_text(text: str, delay=0.1):
     for char in text:
@@ -43,54 +14,14 @@ if __name__ == '__main__':
     # print character sheet
     # specify roll that must be beat and enemy initiative by collecting user input
     # any buffs / debuffs?
-    # calculate player initiatve to see who goes first 
     # any critical success / failure?
-    # draw result
 
     name = input('Name: ')
     role = input('Role: ')
 
-    tav = Tav(name, role)
-    tav.print_character_sheet()
-
-    print()
+    print('Your name is ' + name + ' and your role is ' + role + '.')
     print_dramatic_text('Our adventure begins in a shady tavern ...')
 
-    wins = 0
-    while True:
-        print_dramatic_text('A monster approaches!')
-        print()
-
-        requirement = generate_monster()
-        print()
-
-        buff = input('Press \'a\' for advantage, \'g\' for guidance, & enter to roll: ')
-
-        result = tav.roll(buff)
-
-        if result < requirement:
-            print_dramatic_text('You needed ' + str(requirement) + ', you rolled ' + str(result) + ' ...')
-            print()
-            print_dramatic_text('      G A M E   O V E R')
-            break
-        if result == 1:
-            print_dramatic_text('      CRITICAL FAILURE')
-            print()
-            print_dramatic_text('      G A M E   O V E R')
-            break
-        if result == 20:
-            wins += 1
-            print_dramatic_text('      CRITICAL SUCCESS')
-            print()
-        else:
-            wins += 1
-            print()
-            print_dramatic_text('You needed ' + str(requirement) + ', you rolled ' + str(result) + ' ... well done!')
-
-        # check win condition!
-        if wins >= 3:
-            print()
-            print_dramatic_text(tav.name + ' the ' + tav.role + ' has completed the long journey!')
-            print()
-            print_dramatic_text('          {  fin  }')
-            break
+    input('Press Enter to roll a d20.')
+    roll = random.randint(1, 20)
+    draw_d20(roll)
