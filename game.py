2fe02f67a5a59cbfd4d073955ac8677b0f987c30
brandon mazey <brandon@brandons-MacBook-Air.local>
2025-11-12 08:18:35 -0500

    examples

--- game.py
+++ game.py
@@ -17,19 +17,32 @@ if __name__ == '__main__':
     # any buffs / debuffs?
     # any critical success / failure?
 
+    # name = input('Name: ')
+    # print('Welcome ' + name + ' ...')
+
     player_1 = Player('Angel Fre', 'Doctor', 10)
-    player_1.attack()
+    option = input('Press A to roll advantage or Enter to roll a d20 ')
+    option.lstrip()
+    if option.lower() == 'a':
+        player_1.roll_advantage()
+    else:
+        player_1.roll_d20()
+    # roll = player_1.roll_d20()
+    # print_dramatic_text(player_1.name + ' rolled a ' + str(roll) + ' ...')
+    # player_1.attack()
+    # roll = player_1.roll_advantage()
+    # print_dramatic_text(player_1.name + ' rolled a ' + str(roll) + ' with advantage!')
 
-    player_2 = Player('Miranda', 'Barbarian', 20)
-    player_2.attack()
+    # player_2 = Player('Miranda', 'Barbarian', 20)
+    # player_2.attack()
 
-    name = input('Name: ')
-    role = input('Role: ')
+    # name = input('Name: ')
+    # role = input('Role: ')
 
-    player = Player(name, role, 10)
-    print('Your name is ' + player.name + ' and your role is ' + player.role + '.')
-    print_dramatic_text('Our adventure begins in a shady tavern ...')
+    # player = Player(name, role, 10)
+    # print('Your name is ' + player.name + ' and your role is ' + player.role + '.')
+    # print_dramatic_text('Our adventure begins in a shady tavern ...')
 
-    input('Press Enter to roll a d6.')
-    roll = random.randint(1, 6)
-    draw_d6(roll)
+    # input('Press Enter to roll a d6.')
+    # roll = random.randint(1, 6)
+    # draw_d6(roll)
--- player.py
+++ player.py
@@ -1,9 +1,29 @@
+import random
+from draw import draw_d20, draw_d4
+
 class Player:
     def __init__(self, name: str, role: str, strength: int):
         self.name = name
         self.role = role
         self.strength = strength
         self.hp = 100
+
+    def roll_d20(self) -> int:
+        roll = random.randint(1, 20)
+        draw_d20(roll)
+        return roll
+
+    def roll_advantage(self) -> int:
+        first_roll = random.randint(1, 20)
+        draw_d20(first_roll)
+
+        second_roll = random.randint(1, 20)
+        draw_d20(second_roll)
+
+        if first_roll > second_roll:
+            return first_roll
+        else:
+            return second_roll
 
     def attack(self) -> None:
         print(self.name + " the " + self.role + " attacks!")
